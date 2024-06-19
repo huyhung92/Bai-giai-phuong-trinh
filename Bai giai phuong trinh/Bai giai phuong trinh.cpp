89f67e933301dd553183e79a76892e789aa7ef34
SPC <SPC@33-LAB402>
2024-06-19 19:23:15 +0700

    Add project files.

---
+++ Bai giai phuong trinh/Bai giai phuong trinh.cpp
@@ -0,0 +1,27 @@
+﻿// Bai giai phuong trinh.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b, x;
+    cout << "nhap a";
+    cin >> a;
+    cout << "nhap b";
+    cin >> b;
+    x = -b / a;
+    if (a == 0 && b == 0) {
+        cout << "phuong trinh dung voi bac x";
+    }
+    else if (a == 0 && b != 0) {
+        cout << "vo nghiem";
+    }
+    else if (a != 0) {
+        cout << "nghiem" << x;
+    }
+    return 0;
+}
+
+
+
+    
